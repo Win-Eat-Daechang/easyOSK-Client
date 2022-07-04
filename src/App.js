3c01bafc6fce8c9c52242bd34b60a5971bef6cb6
SEEWON <andyaaa78@gmail.com>
2022-07-05 00:10:25 +0900

    [Feat] Routings

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
 import styled from "styled-components";
+import Account from "./pages/Account";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
 
 const App = () => {
   return (
     <Wrapper className="App">
-      <header className="App-header">
-        Hello, World!
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
     </Wrapper>
   );
 }
 
 export default App;
 
-const Wrapper = styled.section``;
+const Wrapper = styled.div``;
