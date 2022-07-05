82ececaea14a308dafd2a042d0a4e06cddc14c55
SEEWON <andyaaa78@gmail.com>
2022-07-05 23:23:33 +0900

    [Stlye] Code formatting

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,12 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const Result = () => {
-
-  return (
-    <div>
-      Result
-    </div>
-  );
-}
+  return <div>Result</div>;
+};
 
 export default Result;
